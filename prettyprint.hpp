6839a75437ffe433636322e537c9833a80b24b32
Katherine Whitlock <toroidalcode@gmail.com>
2016-04-26 01:46:15 -0400

    Fix build errors for MSVC 14.0

--- prettyprint.hpp
+++ prettyprint.hpp
@@ -184,7 +184,7 @@ namespace pretty_print
     template <typename T1, typename T2>
     struct print_container_helper<T, TChar, TCharTraits, TDelimiters>::printer<std::pair<T1, T2>>
     {
-        using ostream_type = print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
+        using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
 
         static void print_body(const std::pair<T1, T2> & c, ostream_type & stream)
         {
@@ -201,7 +201,7 @@ namespace pretty_print
     template <typename ...Args>
     struct print_container_helper<T, TChar, TCharTraits, TDelimiters>::printer<std::tuple<Args...>>
     {
-        using ostream_type = print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
+        using ostream_type = typename print_container_helper<T, TChar, TCharTraits, TDelimiters>::ostream_type;
         using element_type = std::tuple<Args...>;
 
         template <std::size_t I> struct Int { };
